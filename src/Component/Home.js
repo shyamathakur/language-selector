14854905e59e35f98c1a0bef3a569f2816311bac
shyamathakur <shyamathakur406@gmail.com>
2023-09-14 11:27:38 +0530

    message

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -8,14 +8,14 @@ const Home = () => {
   const { translate } = useLanguage();
 
   return (
-    <div>
+    <div className='back'>
       <div className='button-group-about'>
         <button className='button-col'>{translate('heading-home')}</button>
         <button className='button-col'>
           <Link style={{ color: "#fff", textDecoration: "none" }} to="/about">{translate('heading')}</Link></button>
       </div>
       <div style={{ padding: "10px 20px", textAlign: "justify" }}>
-        <h1 style={{ color: "#6b83af" }}>{translate('heading-home')}</h1>
+        <h1 style={{ color: "#2f5ecb" }}>{translate('heading-home')}</h1>
         <p className='pera'>{translate('home')}</p>
       </div>
     </div>
